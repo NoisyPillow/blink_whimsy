cc253dee7bdb500969797bd9adee14c55fe88339
noisypillow <ycazee@gmail.com>
2022-04-06 22:27:54 +0200

    qzufbibfiuif

--- Whimsy.js
+++ Whimsy.js
@@ -18,5 +18,5 @@ let lightWhite  = '#FFFFFF'; // white
 t.prefs_.set('color-palette-overrides', [black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite]);
 
 t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', white;
+t.prefs_.set('foreground-color', white);
 t.prefs_.set('background-color', black);
