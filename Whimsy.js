8bd87e06618dff621a8020d1f942bc5533f0d1c8
noisypillow <ycazee@gmail.com>
2022-04-06 22:05:53 +0200

    first commit

---
+++ Whimsy.js
@@ -0,0 +1,26 @@
+black       = '#535178';
+red         = '#EF6487'; // red
+green       = '#5ECA89'; // green
+yellow      = '#FDD877'; // yellow
+blue        = '#65AEF7'; // blue
+magenta     = '#AA7FF0'; // pink
+cyan        = '#43C1BE'; // cyan
+white       = '#FFFFFF'; // light gray
+lightBlack  = '#535178'; // medium gray
+lightRed    = '#EF6487'; // red
+lightGreen  = '#5ECA89'; // green
+lightYellow = '#FDD877'; // yellow
+lightBlue   = '#65AEF7'; // blue
+lightMagenta= '#AA7FF0'; // pink
+lightCyan   = '#43C1BE'; // cyan
+lightWhite  = '#FFFFFF'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', black);
