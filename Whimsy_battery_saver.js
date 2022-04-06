54047faac1f21643e688e92583606167c1cd1a4a
noisypillow <ycazee@gmail.com>
2022-04-06 23:14:15 +0200

    added a battery saving version with a black background

---
+++ Whimsy_battery_saver.js
@@ -0,0 +1,5 @@
+t.prefs_.set('color-palette-overrides', ["#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF", "#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF"]);
+
+t.prefs_.set('foreground-color', "#D9D9D9");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
