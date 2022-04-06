f73b6672b3e96d0518484bd6809e08d4422b5adc
noisypillow <ycazee@gmail.com>
2022-04-06 22:17:34 +0200

    ughiseyuvfg

--- Whimsy.js
+++ Whimsy.js
@@ -15,12 +15,8 @@ lightMagenta= '#AA7FF0'; // pink
 lightCyan   = '#43C1BE'; // cyan
 lightWhite  = '#FFFFFF'; // white
 
-t.prefs_.set('color-palette-overrides',
-                 [ black , red     , green  , yellow,
-                  blue     , magenta , cyan   , white,
-                  lightBlack   , lightRed  , lightGreen , lightYellow,
-                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+t.prefs_.set('color-palette-overrides', [black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite]);
 
 t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', white);
-t.prefs_.set('background-color', black);
+t.prefs_.set('foreground-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('background-color', white);
