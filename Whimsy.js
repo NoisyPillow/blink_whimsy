8066a2b7f2897d02d04ce335264a82699eefc450
noisypillow <ycazee@gmail.com>
2022-04-06 23:07:38 +0200

    zqygqytevfuquu

--- Whimsy.js
+++ Whimsy.js
@@ -1,5 +1,5 @@
 t.prefs_.set('color-palette-overrides', ["#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF", "#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF"]);
 
 t.prefs_.set('foreground-color', "#D9D9D9");
-t.prefs_.set('background-color', "#535178");
+t.prefs_.set('background-color', "#0A0A0A");
 t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
