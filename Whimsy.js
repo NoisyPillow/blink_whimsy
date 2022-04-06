5e9500288655b3f75080d6ebc5a7c5f5847c8d73
noisypillow <ycazee@gmail.com>
2022-04-06 22:32:41 +0200

    iehgfugugzeu

--- Whimsy.js
+++ Whimsy.js
@@ -1,22 +1,5 @@
-let black       = '#535178';
-let red         = '#EF6487'; // red
-let green       = '#5ECA89'; // green
-let yellow      = '#FDD877'; // yellow
-let blue        = '#65AEF7'; // blue
-let magenta     = '#AA7FF0'; // pink
-let cyan        = '#43C1BE'; // cyan
-let white       = '#FFFFFF'; // light gray
-let lightBlack  = '#535178'; // medium gray
-let lightRed    = '#EF6487'; // red
-let lightGreen  = '#5ECA89'; // green
-let lightYellow = '#FDD877'; // yellow
-let lightBlue   = '#65AEF7'; // blue
-let lightMagenta= '#AA7FF0'; // pink
-let lightCyan   = '#43C1BE'; // cyan
-let lightWhite  = '#FFFFFF'; // white
+t.prefs_.set('color-palette-overrides', ["#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF", "#535178", "#EF6487", "#5ECA89", "#FDD877", "#65AEF7", "#AA7FF0", "#43C1BE", "#FFFFFF"]);
 
-t.prefs_.set('color-palette-overrides', [black, red, green, yellow, blue, magenta, cyan, white, lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite]);
-
-t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', white);
-t.prefs_.set('background-color', black);
+t.prefs_.set('foreground-color', "#4a4543");
+t.prefs_.set('background-color', "#f7f7f7");
+t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
